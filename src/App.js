2ec9ea9acf82fbd82de681709b5de3867576918b
Nidhi kumari <nidhiaarohi123@gmail.com>
2023-05-18 21:26:37 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+	
+	
+	
+
+	return (
+		<Router>
+			<div className='App'>
+				<Routes>
+					
+				
+				</Routes>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
